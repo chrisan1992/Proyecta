3813da189e2f7d4a4e60fd7b3a9dd0a643c36b25
Gabriel Ulloa <gulloa91@gmail.com>
2013-10-10 02:35:19 -0600

    layout

---
+++ Proyecta/Proyecta/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecta.Models
+{
+    public class IUsuario
+    {
+        [Display(Name = "Cédula")]
+        [Required(ErrorMessage = "required")]
+        [StringLength(20, ErrorMessage = "Contraseña de no más de 20 caracteres")]
+        public string Cedula { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "required")]
+        [StringLength(15, ErrorMessage = "La cédula tiene que ser menor que 15 caracteres")]
+        public string Password { get; set; }
+
+        [Display(Name = "Correo electrónico")]
+        [Required(ErrorMessage = "required")]
+        [StringLength(40, ErrorMessage = "Correo demasiado largo")]
+        public string Correo { get; set; }
+    }
+
+    [MetadataType(typeof(IUsuario))]
+    public partial class Usuario
+    {
+
+    }
+}
